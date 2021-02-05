d9218d588a3e11cf07a386eaeaea7016c826bd32
Игорь Алексеевич <barrayar@MacBook-Air-Igor.local>
2021-02-05 22:11:45 +0300

    reworked after 1st review

--- homework.py
+++ homework.py
@@ -2,14 +2,14 @@ import datetime as dt
 
 
 class Record:
-    def __init__(self, amount=int, comment='', date=None):
+    def __init__(self, amount: int, comment: str, date=None):
         self.amount = amount
         self.comment = comment
-        if not date:
+        if date is None:
             this_date = dt.datetime.now().date()
             self.date = this_date
         else:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+            self.date = dt.datetime.strptime(date, '%d.%m.%y').date()
 
 
 class Calculator:
@@ -17,7 +17,7 @@ class Calculator:
         self.limit = limit
         self.records = []
 
-    def add_record(self, record):
+    def add_record(self, record: Record):
         self.records.append(record)
 
     def get_today_stats(self):
@@ -29,10 +29,9 @@ class Calculator:
 
     def get_week_stats(self):
         week_summary = 0
-        time_delta = dt.timedelta(days=7)
+        week_ago = dt.datetime.now().date() - dt.timedelta(days=7)
         for record in self.records:
-            if (dt.datetime.now().date() - time_delta) <= \
-                    record.date <= dt.datetime.now().date():
+            if week_ago <= record.date <= dt.datetime.now().date():
                 week_summary += record.amount
         return week_summary
 
@@ -45,12 +44,10 @@ class CashCalculator(Calculator):
     def get_today_cash_remained(self, currency):
         cur_output = 0
         cur_name = ''
-        today_expenses = 0
-        for record in self.records:
-            if record.date == dt.datetime.now().date():
-                today_expenses += record.amount
-        remained_cash = self.limit - today_expenses
-        if currency == 'rub':
+        remained_cash = self.limit - self.get_today_stats()
+        if remained_cash == 0:
+            return 'Денег нет, держись'
+        elif currency == 'rub':
             cur_output = abs(remained_cash)
             cur_name = 'руб'
         elif currency == 'usd':
@@ -61,8 +58,6 @@ class CashCalculator(Calculator):
             cur_name = 'Euro'
         if remained_cash > 0:
             return f'На сегодня осталось {cur_output} {cur_name}'
-        elif remained_cash == 0:
-            return 'Денег нет, держись'
         else:
             return 'Денег нет, держись: твой долг - ' \
                     f'{cur_output} {cur_name}'
@@ -70,10 +65,7 @@ class CashCalculator(Calculator):
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        today_calories = 0
-        for record in self.records:
-            if record.date == dt.datetime.now().date():
-                today_calories += record.amount
+        today_calories = self.get_week_stats()
         remained_calories = self.limit - today_calories
         if remained_calories > 0:
             return 'Сегодня можно съесть что-нибудь ещё,' \
